1e84fa13820d1229e0f4b1d9f972310026d959a0
SABARINATHKARMEGAM <111856502+SABARINATHKARMEGAM@users.noreply.github.com>
2022-08-30 23:07:08 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World - 30th August 2022 - Crontab testing !");
+    System.out.println("Hello World - 30th August 2022 - webhook testing !");
     }
 }
